78e3d96346804a78420bc7d214020891c2dd4ebd
SukanyaVee <v.sukanya@gmail.com>
2018-03-01 13:30:12 -0700

    rolling back previous change

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const   express = require('express'),
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(`../build`));
+app.use(express.static(`${_dirname}/../build`));
 
 
 app.post('/message', (req, res) => {
